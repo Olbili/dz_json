f8eb91782cf679e2357880d1d3bc0bd8937ec071
olbili <Olegbilinskiy03@gmail.com>
2023-03-18 14:14:33 +0200

    4

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import createLi from "./js/render";
-
-import Notiflix from "notiflix";
-
 const form = document.querySelector(".form");
 const list = document.querySelector(".list");
 
+const text = document.querySelector(".text");
+
+console.log('text :>> ', text);
+
 form.addEventListener("submit", onSubmit);
+
+console.log('form :>> ', form);
 
 function onSubmit (e) {
     e.preventDefault ();
-
+    
     const {
         "elements": {email, password}
     } = e.currentTarget;
-
+    
+    console.log('email, password :>> ', email, password);
     const markup = createLi(email.value, password.value);
 
     if (email.value === "" && password.value === "") {
@@ -26,4 +29,5 @@ function onSubmit (e) {
     list.insertAdjacentHTML("beforeend", markup);
 
     e.currentTarget.reset();
+
 };
--- src/js/render.js
+++ src/js/render.js
@@ -1,5 +1,5 @@
 export default function createLi (email, password) {
-    const markup = `<li> Email: ${email.value}, Password: ${password.value} </li>`;
+    const markup = `<li> Email: ${email}, Password: ${password} </li>`;
     return markup;
 };
 
